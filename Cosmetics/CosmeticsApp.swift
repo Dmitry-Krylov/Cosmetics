e772da790b7b13525b4540f2c43f796d4211fd4f
Dmitry-Krylov <93715663+Dmitry-Krylov@users.noreply.github.com>
2023-08-03 12:34:41 +0200

    Initial Commit

---
+++ Cosmetics/CosmeticsApp.swift
@@ -0,0 +1,20 @@
+//
+//  CosmeticsApp.swift
+//  Cosmetics
+//
+//  Created by Елена Крылова on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CosmeticsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
